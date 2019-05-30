a8366d99704b173fa730092a39c63cf9edfc9fed
Arjav Lad <ladarjav@gmail.com>
2019-05-30 17:52:22 +0530

    Project setup

---
+++ GiphyExplorer/GiphyExplorer/ViewControllers/GiphyExplorerCollectionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GiphyExplorerCollectionViewController.swift
+//  GiphyExplorer
+//
+//  Created by Arjav Lad on 30/05/19.
+//  Copyright © 2019 Arjav Lad. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class GiphyExplorerCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        self.clearsSelectionOnViewWillAppear = true
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
